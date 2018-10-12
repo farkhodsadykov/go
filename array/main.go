3a4f5c8f0e49fea15c9d02f40617541f1f79137e
farkhodsadykov <sadykovfarkhod@gmail.com>
2018-10-11 19:27:44 -0500

    Modified main.go desk.go

--- array/main.go
+++ array/main.go
@@ -6,13 +6,13 @@ import (
 
 func main() {
 	card := []string{"This is first line", newCard()}
-	card = append(card, "This is first second")
-	fmt.Println(card)
+	card = append(card, "This is second line")
+	// fmt.Println(card)
 	for i, data := range card {
 		fmt.Println(i, data)
 	}
 }
 
 func newCard() string {
-	return "This is first third"
+	return "This is third line"
 }
